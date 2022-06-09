318ed4e023130b21297009ceab351a092aff6d9f
Rifki Amdan <44013588+rifkiiamdan28@users.noreply.github.com>
2022-06-09 18:16:19 +0700

    first commit

---
+++ ExampleApps/ExampleApps/RoundedCornerView.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedCornerView.swift
+//  ExampleApps
+//
+//  Created by Macbook Pro BTS.id on 09/06/22.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedCornerView: UIView {
+
+    // if cornerRadius variable is set/changed, change the corner radius of the UIView
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+
+}
---
+++ ExampleApps/ExampleApps/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ExampleApps
+//
+//  Created by Macbook Pro BTS.id on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
